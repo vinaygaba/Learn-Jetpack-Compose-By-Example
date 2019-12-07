451844a939085ff9a311ffa85652c789a2dab70d
Vinay Gaba <vinaygaba@gmail.com>
2019-12-07 15:30:24 +0530

    Bump up compose version

---
+++ app/src/main/java/com/example/jetpackcomposeplayground/CustomTextActivity.kt
@@ -0,0 +1,33 @@
+package com.example.jetpackcomposeplayground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+class CustomTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun DefaultPreview2() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+}
