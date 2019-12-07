f0a58f174f62edd7e955405d93d11afa94a37806
Vinay Gaba <vinaygaba@gmail.com>
2019-12-07 12:12:36 +0530

    Added first example

--- app/src/main/java/com/example/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.jetpackcomposeplayground
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startSimpleTextExample(view: View) {
+        startActivity(Intent(this, SimpleTextActivity::class.java))
     }
 }
--- app/src/main/java/com/example/jetpackcomposeplayground/SimpleTextActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeplayground/SimpleTextActivity.kt
@@ -5,29 +5,44 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
-import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 
-class ComposeActivity : AppCompatActivity() {
+class SimpleTextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // This is an extension function of Activity that sets the @Composable function that's
+        // passed to it as the root view of the activity. This is meant to replace the .xml file
+        // that we would typically set using the setContent(R.id.xml_file) method. The setContent
+        // block defines the activity's layout.
         setContent {
-            MaterialTheme {
-                Greeting("Android")
-            }
+            // Here, Message is a Composable function which is going to describe the contents of
+            // this activity that will be rendered on the screen.
+            Message("This is the Jetpack Compose Playground")
         }
     }
 }
 
+// We represent a Composable function by annotating it with the @Composable annotation. Composable
+// functions can only be called from within the scope of other composable functions.
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Message(displayText: String) {
+    // We should think of composable functions to be similar to lego blocks - each composable
+    // function is in turn built up of smaller composable functions. Here, the Text() function is
+    // pre-defined by the Compose UI library; you call that function to declare a text element
+    // in your app.
+    Text(text = displayText)
 }
 
+
+// Android Studio lets you preview your composable functions within the IDE itself, instead of
+// needing to download the app to an Android device or emulator. This is a fantastic feature as you
+// can preview all your custom components(read composable functions) from the comforts of the IDE.
+// The main restriction is, the composable function must not take any parameters. If your composable
+// function requires a parameter, you can simply wrap your component inside another composable
+// function that doesn't take any parameters and call your composable function with the appropriate
+// params. Also, don't forget to annotate it with @Preview & @Composable annotations.
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Moto")
-    }
+    Message("This is the Jetpack Compose Playground")
 }
