a3ea58625f22b9545c190f03940da15c94fb335b
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-12-12 01:56:53 +0530

    Setup NavigationActivity.

---
+++ app/src/main/java/com/example/jetpackcompose/navigation/ComposeNavigationActivity.kt
@@ -0,0 +1,16 @@
+package com.example.jetpackcompose.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ComposeNavigationActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
