b5e23ef9079c72fa3c5ec7a024dcb840b0e386a7
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-01-09 23:54:08 +0530

    Add tasks DataSource.

---
+++ app/src/main/java/com/example/jetpackcompose/navigation/Datasource.kt
@@ -0,0 +1,38 @@
+package com.example.jetpackcompose.navigation
+
+import java.util.Calendar
+import java.util.Date
+
+data class Task(
+    val id: Int,
+    val title: String,
+    val description: String? = null,
+    val timestamp: Date? = Calendar.getInstance().time, // I haven't used LocalDateTime as de-sugaring is another concept altogether
+)
+
+object Datasource {
+
+    private val tasks = listOf(
+        Task(
+            id = 1,
+            title = "Contribute to Learn-Jetpack-Compose-By-Example",
+            description = "Pick a simple issue and try to solve it.",
+        ),
+        Task(
+            id = 2,
+            title = "Binge Batman Trilogy",
+        ),
+        Task(
+            id = 3,
+            title = "Buy groceries.",
+            description = "Don't forget to buy bread and butter!",
+            timestamp = Calendar.getInstance().time
+        ),
+        Task(
+            id = 4,
+            title = "Clear out weeds from the backyard",
+        ),
+    )
+
+    fun getAllTasks() = tasks
+}
